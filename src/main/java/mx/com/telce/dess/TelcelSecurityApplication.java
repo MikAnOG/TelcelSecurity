01d1dd99bfe60bbdaa629347458fe0abb52c7709
Rafael Flores Islas <62164718+RFlores-Islas@users.noreply.github.com>
2022-03-21 23:16:20 -0600

    First commit

---
+++ src/main/java/mx/com/telce/dess/TelcelSecurityApplication.java
@@ -0,0 +1,13 @@
+package mx.com.telce.dess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelcelSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelcelSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/telce/dess/TelcelSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.telce.dess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelcelSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
